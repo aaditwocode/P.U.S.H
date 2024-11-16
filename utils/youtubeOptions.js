3b92b8b5d52141088703ee102368b6861b189d72
SinghalSahab <prakharsinghal10a@gmail.com>
2024-11-17 04:32:19 +0530

    done with api

--- utils/youtubeOptions.js
+++ utils/youtubeOptions.js
@@ -2,7 +2,7 @@ export const youtubeOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': '50f16bf47amshd0990582e7d2205p1d59dcjsncae1f55f3577',
+      'X-RapidAPI-Key': '50f16bf47amshd0990582e7d2205p1d59dcjsncae1f55f357',
     },
   };
   
